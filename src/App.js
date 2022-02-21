a3080a67091ac5d531724f74a4944ac3aceb0b2d
emiliaaslund <emilia.aslund@cmeducations.se>
2022-02-21 15:12:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./index.js";
+import "./App.css";
+
+//components
+import ToDoList from "./components/ToDoList";
+import InputBox from "./components/InputBox";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(title) {
+    setItems((prevItems) => {
+      return [
+        ...prevItems,
+        {
+          id: uuidv4(),
+          title: title,
+        },
+      ];
+    });
+  }
+
+  function removeItem(id) {
+    const newItem = [...items].filter((item) => item.id !== id);
+    setItems(newItem);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">MY TO DO LIST</h1>
+      <InputBox addTitle={addItem} />
+      <ToDoList addItem={items} removeItem={removeItem} />
     </div>
   );
 }
